c5d7deaafb595e4d25238640538b64daf23d0804
Andrew Marcuse <fileformat@gmail.com>
2025-01-25 18:33:19 -0500

    Removed unnecessary react imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
